195adcda05ac4f9846c58d8bfa8a51cb4f4dace1
Abhishek Thakur <th.abhishek12@gmail.com>
2022-01-28 11:34:01 +0530

    new project on react with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List"
+import AddToList from './components/AddToList';
 
+export interface IState{
+  people:{
+    name: string
+    age: number
+    url: string
+    note ?: string
+  }[]
+}
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Rohit Sharma",
+      age: 33,
+      url: "https://cricket.upcomingwiki.com/public/images/gallery/26551.png",
+      note: "Hitman of team India"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to the party</h1>
+      <List people = {people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
